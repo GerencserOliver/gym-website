163d105f6336e4bb6e0d5f58a405d700c13e95ad
GerencserOliver <gerencser.oliver@students.jedlik.eu>
2024-04-04 21:45:04 +0200

    Changing images

--- components/Services.jsx
+++ components/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ServiceCard = ({ imageUrl, title }) => {
     return (
-        <div className='relative rounded-2xl shadow-lg cursor-pointer w-96 sm:w-72'>
+        <div className='relative rounded-2xl shadow-lg cursor-pointer w-96 sm:w-72 grayscale hover:grayscale-0 transition ease-in-out'>
             <img src={imageUrl} alt={title} className='rounded-2xl object-cover h-48 sm:h-72 w-80 mx-auto' />
             <div className='absolute inset-0 flex justify-center items-center'>
                 <div>
@@ -20,10 +20,10 @@ const Services = () => {
                 <div className='container mx-auto'>
                     <h2 className='text-6xl font-bold text-center text-white'>We offer something for everybody</h2>
                     <div className='flex justify-center items-center flex-wrap gap-8 mt-16 text-center'>
-                        <ServiceCard imageUrl="/card-1.png" title="Personal Training" />
-                        <ServiceCard imageUrl="/card-1.png" title="Yoga Classes" />
-                        <ServiceCard imageUrl="/card-1.png" title="Nutrition Consultation" />
-                        <ServiceCard imageUrl="/card-1.png" title="Group Workouts" />
+                        <ServiceCard imageUrl="/personal-training.png" title="Personal Training"/>
+                        <ServiceCard imageUrl="/yoga.png" title="Yoga Classes" />
+                        <ServiceCard imageUrl="/nutrition.png" title="Nutrition Consultation" />
+                        <ServiceCard imageUrl="/group-workout.png" title="Group Workouts" />
                     </div>
                 </div>
             </section>
--- components/Trainers.jsx
+++ components/Trainers.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const TrainerCard = ({ imageSrc, name, facebook, email, instagram }) => {
     return (
-        <div className='bg-[#1F1F1F] rounded-xl p-8 w-80'>
-            <img src={imageSrc} alt="Trainer" className='w-32 h-32 rounded-full mx-auto' />
+        <div className='bg-[#1F1F1F] rounded-xl p-16'>
+            <img src={imageSrc} alt="Trainer" className='w-64 h-64 rounded-full mx-auto' />
             <h3 className='text-[#DCCA87] font-bold text-2xl mt-4'>{name}</h3>
             <div className='flex justify-center items-center gap-4 mt-4'>
                 <a href={facebook} target='_blank' rel='noreferrer'>
@@ -31,21 +31,21 @@ const Trainers = () => {
             <p className='text-[#FFFFFF] text-xl mt-10 text-center max-w-lg mx-auto'>Expert team of coaches helps you succeed in any goal, personalized guidance and motivation provided!</p>
             <div className='flex justify-center items-center flex-wrap gap-16 mt-16 text-center'>
                 <TrainerCard 
-                    imageSrc='/card-1.png' 
+                    imageSrc='/trainer-1.png' 
                     name='John Doe' 
                     facebook='https://www.facebook.com/johndoe'
                     email='johndoe@example.com'
                     instagram='https://www.instagram.com/johndoe'
                 />
                 <TrainerCard 
-                    imageSrc='/card-1.png' 
+                    imageSrc='/trainer-2.png' 
                     name='Jane Doe' 
                     facebook='https://www.facebook.com/janedoe'
                     email='janedoe@example.com'
                     instagram='https://www.instagram.com/janedoe'
                 />
                 <TrainerCard 
-                    imageSrc='/card-1.png' 
+                    imageSrc='/trainer-3.png' 
                     name='John Smith' 
                     facebook='https://www.facebook.com/johnsmith'
                     email='johnsmith@example.com'
