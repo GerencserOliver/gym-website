40beaa2eeea98a53ba6898261cea2e714cbd8f28
GerencserOliver <gerencser.oliver@students.jedlik.eu>
2024-04-11 20:02:40 +0200

    Schedule

--- app/schedule/page.jsx
+++ app/schedule/page.jsx
@@ -3,20 +3,30 @@ import React, { useState } from 'react';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
-const Classes = ({ trainingClass, time, trainer, Join }) => {
+const Classes = ({ trainingClass, time, trainer }) => {
   return (
-    <ul className=''>
-      <li className="flex p-4 border-white" style={{ marginBottom: '1rem', paddingTop: '1rem', paddingBottom: '1rem' }}>
-        <p>{trainingClass}</p>
-        <p>{time}</p>
-        <p>{trainer}</p>
-        <button className='bg-black text-white px-6 py-3 rounded-md hover:bg-[#cfc292] duration-300'>
-          Join
-        </button>
-      </li>
-    </ul>
+    <div className="flex flex-col md:flex-row items-center justify-between w-10/12 mx-auto my-1 p-6 rounded-xl bg-[#F2F2F2]">
+      <div className='flex flex-col md:flex-row gap-4 md:gap-12'>
+        <div className="text-center md:text-left">
+          <p className='text-sm text-gray-500'>Class name</p>
+          <h1 className="text-2xl font-bold">{trainingClass}</h1>
+        </div>
+        <div className="text-center md:text-left">
+          <p className='text-sm'>Time</p>
+          <h1 className="text-2xl font-bold">{time}</h1>
+        </div>
+        <div className="text-center md:text-left">
+          <p className='text-sm'>Trainer</p>
+          <h1 className="text-2xl font-bold">{trainer}</h1>
+        </div>
+      </div>
+      <button className="bg-[#DCCA87] text-white p-2 px-12 rounded-3xl hover:bg-white hover:text-[#DCCA87] duration-300 w-full md:w-auto mt-4 md:mt-0">
+        Join
+      </button>
+    </div>
   );
 }
+
 
 const Schedule = () => {
   const [currentDay, setCurrentDay] = useState('Monday');
@@ -81,17 +91,17 @@ const Schedule = () => {
         </h1>
       </div>
       <div className='flex items-center justify-center mt-24 flex-wrap'>
-        {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(day => (
-          <button
-            key={day}
-              className={`mx-2 my-2 text-xl font-bold text-black bg-white p-2 rounded-3xl hover:bg-[#DCCA87] hover:text-white duration-300 ${currentDay == day ? 'bg-[#DCCA87] text-white' : ''}`}
-            onClick={() => handleDayChange(day)}
-          >
-            {day}
-          </button>
-        ))}
+      {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(day => (
+        <button
+          key={day}
+          className={`mx-2 my-2 text-xl font-bold text-black bg-white p-2 px-4 rounded-3xl hover:bg-[#DCCA87] hover:text-white duration-300 ${currentDay == day ? 'current-day' : ''}`}
+          onClick={() => handleDayChange(day)}
+        >
+          {day}
+        </button>
+      ))}
       </div>
-      <div className="flex flex-col items-center justify-center mt-8 text-black text-xl font-bold bg-[#DCCA87] w-3/4 mx-auto">
+      <div className="flex flex-col items-center justify-center mt-8 text-black text-xl w-3/4 mx-auto mb-24">
         {classesByDay[currentDay].map((classInfo, index) => (
           <Classes
             key={index}
