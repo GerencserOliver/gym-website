4014217f8b3df3e94fb6dd7ff5f7728f74efa8c4
GerencserOliver <gerencser.oliver@students.jedlik.eu>
2024-04-17 20:07:37 +0200

    About page, footer update

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,21 +1,40 @@
-import Navbar from '@/components/Navbar';
-import AboutComp from '@/components/AboutComp';
 import React from 'react';
+import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Message from '@/components/Message';
 
 const About = () => {
   return (
     <div>
       <div className="login-banner relative justify-center flex">
-          <h1 className="text-white absolute bottom-10 text-5xl font-bold">
-            About
-          </h1>
+        <h1 className="text-white absolute bottom-10 text-5xl font-bold">
+          About
+        </h1>
       </div>
+      <div className="container max-w-screen-xl mx-auto my-10 mb-24 p-12">
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+          <div className='mt-24'>
+            <h1 className="text-5xl text-[#DCCA87] font-bold">
+              Who We Are
+            </h1>
+            <h2 className='text-3xl text-white mt-6 font-bold'>
+              We Will Give You Strength and Health
+            </h2>
+            <p className="text-lg text-white mt-8">
+              At Our gym, we are dedicated to helping you achieve the body
+              of your dreams. Our expert trainers and nutritionists will
+              work with you to create a personalized fitness and nutrition
+              plan that helps you reach your specific goals.
+            </p>
+            <button className="mt-8 px-8 py-3 text-lg font-semibold text-white bg-[#DCCA87] rounded-lg hover:bg-[#cfc292] transition-all">Contact us</button>
+          </div>
+          <div className='flex justify-center md:justify-end'>
+            <img className="max-w-full" src="/about-img.png" alt="About us" />
+          </div>
+        </div>
+      </div>
+      <Message />
       <Navbar />
-      <AboutComp />
-      <div className='items-center flex justify-center'>
-        <button className='mt-8 px-8 py-3 text-lg font-semibold text-white bg-[#DCCA87] rounded-lg hover:bg-[#cfc292] transition-all'>{"Contact us ->"}</button>
-      </div>
       <Footer />
     </div>
   );
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,40 +7,40 @@ const Footer = () => {
 
     return (
         <div>
-            <footer className='bg-[#0C0C0C] text-white py-12 md:px-0 px-4'>
+            <footer className='bg-[#0C0C0C] text-white py-12 md:px-0 px-4 max-w-screen-xl m-auto'>
                 <div className='container mx-auto'>
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
-                        <div className='md:order-1'>
-                            <div className='flex justify-center items-center'>
+                        <div className='md:order-1 grid grid-flow-row auto-rows-max justify-center items-center'>
+                            <div>
                                 <Image src='/logo.png' alt='Logo' width={150} height={50}/>
                             </div>
-                            <p className='mt-4 max-w-sm md:max-w-full text-center'>Take your health and body to the next level with our comprehensive program designed to help you reach your fitness goals.</p>
-                            <div className='gap-4 mt-4 flex justify-center items-center'>
+                            <p className='mt-4 max-w-sm md:max-w-full'>Take your health and body to the next level with our comprehensive program designed to help you reach your fitness goals.</p>
+                            <div className='mt-4'>
                                 <a href='#'>
-                                    <FontAwesomeIcon icon={['fab', 'facebook']} className='text-[#DCCA87] text-2xl bg-black p-2 rounded-full transition duration-300 hover:bg-[#DCCA87] hover:text-black' />
+                                    <FontAwesomeIcon icon={['fab', 'facebook']} className='text-[#DCCA87] text-2xl bg-black p-2 rounded-full transition duration-300 hover:bg-[#DCCA87] hover:text-black mr-4' />
                                 </a>
                                 <a href='#'>
-                                    <FontAwesomeIcon icon={['fab', 'instagram']} className='text-[#DCCA87] text-2xl bg-black p-2 rounded-full transition duration-300 hover:bg-[#DCCA87] hover:text-black' />
+                                    <FontAwesomeIcon icon={['fab', 'instagram']} className='text-[#DCCA87] text-2xl bg-black p-2 rounded-full transition duration-300 hover:bg-[#DCCA87] hover:text-black mr-4' />
                                 </a>
                                 <a href='#'>
                                     <FontAwesomeIcon icon={['fas', 'envelope']} className='text-[#DCCA87] text-2xl bg-black p-2 rounded-full transition duration-300 hover:bg-[#DCCA87] hover:text-black' />
                                 </a>
                             </div>
                             <div>
-                                <p className='mt-4 text-center'>Privacy Policy | © {currentYear} Gym</p>
+                                <p className='mt-4'>Privacy Policy | © {currentYear} Gym</p>
                             </div>
                         </div>
-                        <div className='md:order-2 text-center'>
-                            <h3 className='text-xl font-bold'>Quick Links</h3>
+                        <div className='md:order-2 grid grid-flow-row auto-rows-max justify-center items-center'>
+                            <h3 className='text-xl font-bold'>Our Classes</h3>
                             <ul className='mt-5'>
-                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>Home</a></li>
-                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>About</a></li>
-                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>Services</a></li>
-                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>Contact</a></li>
-                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>Contact</a></li>
+                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>Fitness Classes</a></li>
+                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>Aerobics Classes</a></li>
+                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>Power Yoga</a></li>
+                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>Learn Machines</a></li>
+                                <li className='mb-3'><a href='#' className='text-[#DCCA87] hover:text-white duration-100'>Full-body Strength</a></li>
                             </ul>
                         </div>
-                        <div className='md:order-3 text-center'>
+                        <div className='md:order-3 grid grid-flow-row auto-rows-max md:justify-end items-center justify-center'>
                             <h3 className='mb-5 text-xl font-bold'>Working Hours</h3>
                             <div>
                                 <h3 className='mb-3 font-bold text-[#DCCA87]'>Monday - Friday:</h3>
